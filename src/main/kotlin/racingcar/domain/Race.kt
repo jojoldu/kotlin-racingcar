aa57208892529fc0378c32b33a61437aaa2b9597
jojoldu <jojoldu@gmail.com>
2022-05-16 11:57:24 +0900

    클래스 선언부 정리

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -3,13 +3,13 @@ package racingcar.domain
 class Race(private val cars: List<Car>) {
     constructor(carCount: Int) : this(List(carCount) { Car() })
 
-    fun run() {
-        cars.forEach { it.move() }
-    }
-
     val movingDistances: List<Int>
         get() = this.cars.map { it.position }
 
     val carsSize: Int
         get() = this.cars.size
+
+    fun run() {
+        cars.forEach { it.move() }
+    }
 }
